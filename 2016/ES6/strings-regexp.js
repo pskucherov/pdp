b3995a9172a27910bdc9c5d08c55cdb3eedd2147
Pavel Kucherov <pskucherov@yandex.ru>
2016-01-25 21:42:56 +0300

    Со строками разобрался, работа с utf разочаровала, половина не работает, длина строки всё так же в два раза больше, но всё остальное — огонь!

--- 2016/ES6/strings-regexp.js
+++ 2016/ES6/strings-regexp.js
@@ -28,6 +28,16 @@ if (hasRegExpU()) {
     console.log(pattern.test(text));     // true
 }
 
+function codePointLength(text) {
+    var result = text.match(/[\s\S]/gu);
+    return result ? result.length : 0;
+}
+
+console.log(codePointLength("abc"));    // 3
+console.log(codePointLength("𠮷bc"));   // 3 Ожидалось 3, а оно всё равно 4 :(
+
+
+
 // Работа с подстроками
 
 var msg = "Hello world!";
@@ -43,3 +53,69 @@ console.log(msg.includes("x"));             // false
 console.log(msg.startsWith("o", 4));        // true
 console.log(msg.endsWith("o", 8));          // true
 console.log(msg.includes("o", 8));          // false
+
+
+// Repeat!
+console.log("x".repeat(3));         // "xxx"
+console.log("hello".repeat(2));     // "hellohello"
+console.log("abc".repeat(4)); // abcabcabcabc
+
+// В регулярки добавился флаг 'y', но там мистика какая-то.
+// Чёт как-то даже на ум не пришло, где это можно заиспользовать в жизни.
+
+
+// Можно дополнять регулярки флагами
+var re1 = /ab/i,
+    // throws an error in ES5, okay in ES6
+    a; //re2 = new RegExp(re1, "g"); Правда у меня здесь падает :)))
+
+// В ES5, чтобы получить флаги, надо было приводить к строке и парсить регулярку
+// в ES6 есть методы получения содержимого регулярки
+var re = /ab/g;
+
+console.log(re.source);     // "ab"
+console.log(re.flags);      // "g"
+
+// Ураааа!!! :)
+console.log("Multiline \
+    string");
+console.log(`Multiline
+    string`);
+
+var name = "Nicholas",
+    message = 'Hello, ${name}.';
+
+console.log(message);       // "Hello, Nicholas."
+
+
+// Огонь!
+// Можно обрабатывать содержимое шаблона в методе.
+function passthru(literals, ...substitutions) {
+  let result = "";
+
+    // run the loop only for the substitution count
+    for (let i = 0; i < substitutions.length; i++) {
+        result += literals[i];
+        result += +substitutions[i] + 3;
+    }
+
+    // add the last literal
+    result += literals[literals.length - 1];
+
+    return result;
+}
+
+var count = 10,
+    price = 0.25,
+    message = passthru`${count} items cost $${(count * price).toFixed(2)}.`;
+
+console.log(message);       // "10 items cost $2.50." ... а вот и нет ;)
+
+
+// Экранирование спецсимволов
+let message1 = `Multiline\nstring`,
+    message2 = String.raw`Multiline\nstring`;
+
+console.log(message1);          // "Multiline
+                                //  string"
+console.log(message2);          // "Multiline\\nstring"
